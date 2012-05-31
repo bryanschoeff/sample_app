3ffe48ed002c378f69c17764de7f174a640163a3
cretan <schoeff@gmail.com>
2012-05-31 14:54:43 -0400

    Added static files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 SampleApp::Application.routes.draw do
+  get "static/home"
+
+  get "static/help"
+
+  get "static/about"
+
+  get "static/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
