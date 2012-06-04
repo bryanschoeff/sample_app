7b80bd27e1fee5e18592d14896c9ac5f3d400121
cretan <schoeff@gmail.com>
2012-06-04 12:56:05 -0400

    finish signup form

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  
+  # Returns the gravitar for the given user
+  def gravitar_for(user, options = { size: 50 })
+    gravitar_id = Digest::MD5::hexdigest(user.email.downcase)
+    
+    size = options[:size]
+    gravitar_url = "http://gravatar.com/avatar/#{gravitar_id}?s=#{size}"
+    image_tag(gravitar_url, alt: user.name, class: "gravitar")
+  end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,4 +11,42 @@ describe "User pages" do
     it { should have_selector('title', text: 'Ruby on Rails Sample Application :: Sign Up')}
   end
   
+  describe "signup page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user)}
+    
+    it { should have_selector('h1', text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+  
+  describe "signup" do
+    
+    before { visit signup_path }
+    
+    describe "with invalid information" do
+      it "should not create a user" do
+        old_count = User.count
+        click_button "Create my account"
+        new_count = User.count
+        new_count.should == old_count
+      end
+    end
+    
+    describe "with valid information" do
+      it "should create a user" do
+        old_count = User.count
+        
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+        
+        click_button "Create my account"
+        new_count = User.count
+        new_count.should == old_count + 1
+      end
+    end
+    
+  end
+  
 end
